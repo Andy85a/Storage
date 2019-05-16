7db3076525d28b81ae89f6e72775953cedd8f2a3
Andreas Eklund <andreas.eklund85@gmail.com>
2019-05-16 09:27:24 +0200

    Add project files.

---
+++ Storage/Migrations/20190514131138_Required.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Storage.Migrations
+{
+    public partial class Required : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Product",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Product",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Product",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ Storage/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Storage.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        [StringLength (30, MinimumLength =1, ErrorMessage ="Name should be at least 1 character long")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [Range(0,50000, ErrorMessage ="Price is out of range")]
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime OrderDate { get; set; }
+        [Required(ErrorMessage ="Category{0} is required")]
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Description { get; set; }
+
+    }
+}
